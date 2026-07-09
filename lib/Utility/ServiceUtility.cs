e6623800183c31bd2b3e3a9fa3b88acda72ae3ef
Hank <eqpc@hbn.dev>
2026-07-09 14:05:16 +0200

    ServiceUtility added

---
+++ lib/Utility/ServiceUtility.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using No1.Commons.Exceptions;
+
+namespace No1.asp.NET.Commons.Utility;
+
+public static class ServiceUtility
+{
+	public static T GetNeededService<T>(this IServiceProvider serviceProvider)
+	where T : notnull {
+		return NullExpressionException.Exec(() => serviceProvider.GetRequiredService<T>());
+	}
+}
